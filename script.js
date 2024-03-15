02f67b24c597869f141c0ed0e3978f5020065969
Rohit Anchal <105239963+rohitanchal@users.noreply.github.com>
2024-03-15 22:32:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function firstWord(s) {
-  // your code here
+  // If the string is empty or doesn't contain any space, return the entire string
+  if (s === '' || !s.includes(' ')) {
+    return s;
+  }
+
+  // Find the index of the first space
+  const spaceIndex = s.indexOf(' ');
+
+  // Extract the substring up to the first space
+  const first = s.substring(0, spaceIndex);
+
+  return first;
 }
 
 // Do not change the code below
